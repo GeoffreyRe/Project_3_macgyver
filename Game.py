4e2d683936bbccd401d893b9e29d583a4da0210c
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2018-12-17 13:15:45 +0100

    création de la classe Game + diverses méthodes associées à celle ci

--- Game.py
+++ Game.py
@@ -4,50 +4,51 @@ from Player import *
 from Item import *
 class Game(object):
 	def __init__(self):
-		display = Display()
-		map_player = Map()
-		needle = Item("needle", 4, map_player)
-		map_player.put_item_on_map(needle)
-		ether = Item("ether", 5, map_player)
-		map_player.put_item_on_map(ether)
-		tube = Item("tube",6,map_player)
-		map_player.put_item_on_map(tube)
-		mac_gyver = Player(map_player)
-		display.load_wall()
-		display.load_ground()
-		display.load_player_display()
-		display.load_jailer()
-		display.load_items()
-		display.refresh(map_player)
-		self.items_in_game = [needle,ether,tube]
+		self.display = Display()
+		self.map_player = Map()
+		self.needle = Item("needle", 4, self.map_player)
+		self.map_player.put_item_on_map(self.needle)
+		self.ether = Item("ether", 5, self.map_player)
+		self.map_player.put_item_on_map(self.ether)
+		self.tube = Item("tube",6,self.map_player)
+		self.map_player.put_item_on_map(self.tube)
+		self.mac_gyver = Player(self.map_player)
+		self.methods_display = [self.display.load_wall, self.display.load_ground,self.display.load_player_display,self.display.load_jailer,self.display.load_items]
+		self.display.load_wall()
+		self.display.load_ground()
+		self.display.load_player_display()
+		self.display.load_jailer()
+		self.display.load_items()
+		self.display.refresh(self.map_player)
+		self.items_in_game = [self.needle,self.ether,self.tube]
 		self.keyboard_list_movements = [pygame.K_UP, pygame.K_DOWN,pygame.K_LEFT, pygame.K_RIGHT]
-		self.keyboard_functions = [mac_gyver.move_up, mac_gyver.move_down, mac_gyver.move_left, mac_gyver.move_right]
+		self.keyboard_functions = [self.mac_gyver.move_up, self.mac_gyver.move_down, self.mac_gyver.move_left, self.mac_gyver.move_right]
 
-	def press_keyboard_movement(self,player,map_player,keyboard_value,display):
+	def press_keyboard_movement(self,keyboard_value):
 		for index, k in enumerate(self.keyboard_list_movements):
 			if k == keyboard_value:
-				player.self.keyboard_functions[index](map_player)
-				display.refresh(map_player)
+				self.keyboard_functions[index](self.map_player)
+				self.display.refresh(self.map_player)
 
-	def press_keyboard_items(self, player,map_player, display):
+	def press_keyboard_items(self):
 		for item in self.items_in_game:
-			if player.player_rect.colliderect(item.item_rect) : # the player can retrieve an item only if the player is close to the item
+			if self.mac_gyver.player_rect.colliderect(item.item_rect) : # the player can retrieve an item only if the player is close to the item
 					if item.on_map: # check if the item is on the map or not
-						player.retrieve_item(item) # the player retrieve the item
+						self.mac_gyver.retrieve_item(item) # the player retrieve the item
 						item.go_to_inventory() #on_map = False
-						map_player.delete_item(item) # item disappears of the map_list
-						display.refresh(map) #update of the display
+						self.map_player.delete_item(item) # item disappears of the map_list
+						self.display.refresh(self.map_player) #update of the display
 
-	def victory(self,player,map_player):
-		if player.position_player_index == map_player.find_jailer():
-			if len(player.inventory) == 3:
+	def victory(self):
+		if self.mac_gyver.position_player_index == self.map_player.find_jailer():
+			if len(self.mac_gyver.inventory) == 3:
 				print("you win !")
+				return False
 			else:
 				print("you died !")
-
-
-
-
+				return False
+		else:
+			return True
 
 
 	def loop(self):
@@ -59,13 +60,12 @@ class Game(object):
 
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_q:
-						self.press_keyboard_items(mac_gyver,map_player, display)
+						self.press_keyboard_items()
 
 					elif event.key == pygame.K_a:
-						self.victory(mac_gyver,map_player)
-						launched = False
-
+						launched = self.victory()
+						
 					else:
-						self.press_keyboard_movement(mac_gyver,map_player,event.key,display)
+						self.press_keyboard_movement(event.key)
 
 
